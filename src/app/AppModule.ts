548526fc2c039ffa23867e41fb02c91edbc4cc55
IEVN-1001-20001055 <144499039+IEVN-1001-20001055@users.noreply.github.com>
2023-11-03 18:53:35 -0600

    CGBT-03-11-23

---
+++ src/app/AppModule.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MaterialModule } from './material/material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
